5a52109076226cb97985c7a3bc59d4aa130f23ea
Graziele Almeida <graaziele.182@gmail.com>
2026-02-08 16:03:49 -0300

    Refatorando a validacao do fluxo de consulta de pedido aprovado

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,24 +1,31 @@
 import { test, expect } from '@playwright/test'
 
-// AAA - Arrange, Act, Assert (Dado, Quando, Então) PREPARAR, AGIR, VERIFICAR
+// AAA - Arrange, Act, Assert
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
 
   // Arrange - preparar o cenário
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+  
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   // Act - agir no cenário
-  await page.getByTestId('search-order-id').fill('VLO-YAF0XN')
-
-  await page.getByTestId('search-order-button').click()
+  await page.getByPlaceholder('Ex: VLO-ABC123').fill('VLO-YAF0XN')
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
   
   // Assert - verificar o resultado
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-YAF0XN')
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  // await page.waitForTimeout(10000) // timeout implicito
+  // await expect(page.getByText('Pedido', { exact: true })).toBeVisible({ timeout: 30_000 }) //30 segundos - timeout explicito 
+
+  // await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 10_000 })
+  // await expect(page.getByTestId('order-result-id')).toContainText('VLO-YAF0XN')
+
+  await expect(page.getByText('Pedido', { exact: true })).toBeVisible({ timeout: 10_000 })
+  await expect(page.getByText('VLO-YAF0XN')).toBeVisible();
+
+  await expect(page.getByText('APROVADO')).toBeVisible() 
+
 })
